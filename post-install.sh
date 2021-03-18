7751b8982df24e08fdcc02abf057cf6074bd375d
Thanh Nguyen <nnthanh101@gmail.com>
2021-03-18 21:24:10 +0700

    no message

--- post-install.sh
+++ post-install.sh
@@ -69,3 +69,45 @@ POLICY=$(cat << EOF
 EOF
 ); \
 aws kms put-key-policy --key-id "${YourKMSArn}" --policy-name default --policy "$POLICY"
+
+## FIXME 2
+
+aws cognito-idp sign-up --region ${AWS_REGION} --client-id $YourUserPoolAppClientId --username nnthanh101@gmail.com --password Passw0rd!
+
+echo "Note: Cognito Confirm Email"
+aws cognito-idp initiate-auth --auth-flow USER_PASSWORD_AUTH --client-id $YourUserPoolAppClientId --auth-parameters USERNAME=nnthanh101@gmail.com,PASSWORD=Passw0rd!
+
+curl -X POST \
+ $YourPaymentMethodApiURL/order     \
+-H 'Authorization: $YourIdToken'    \
+-H 'Content-Type: application/json' \
+-d '{
+"CustomerOrder": "123456789",
+"CustomerName": "Amazon Web Services",
+"CreditCard": "0000-0000-0000-0000",
+"Address": "Unicorn Gym - APJ 2021"
+}'
+
+curl -X POST \
+ $YourPaymentMethodApiURL/paybill \
+-H 'Authorization: $YourIdToken' \
+-H 'Content-Type: application/json' \
+-d '{
+"CustomerOrder": "123456789"
+}'
+
+## Reference FYI ONLY
+
+# YOUR_COGNITO_REGION=ap-southeast-2
+# YOUR_COGNITO_APP_CLIENT_ID=1mmmmubo6107id5fj5t6bpvvjl
+# YOUR_EMAIL=nnthanh101@gmail.com
+# YOUR_PASSWORD=@unicorn-gym-m2m
+
+# echo "Sign up new cognito User"
+# echo aws cognito-idp sign-up \--region $YOUR_COGNITO_REGION \--client-id $YOUR_COGNITO_APP_CLIENT_ID \--username $YOUR_EMAIL \--password $YOUR_PASSWORD
+
+# echo "Verify User"
+# echo aws cognito-idp confirm-sign-up \--client-id $YOUR_COGNITO_APP_CLIENT_ID \--username $YOUR_EMAIL \--confirmation-code CONFIRMATION_CODE_IN_EMAIL
+
+# echo "Get Id Token"
+# echo aws cognito-idp initiate-auth --auth-flow USER_PASSWORD_AUTH --client-id $YOUR_COGNITO_APP_CLIENT_ID --auth-parameters USERNAME=$YOUR_EMAIL,PASSWORD=$YOUR_PASSWORD
